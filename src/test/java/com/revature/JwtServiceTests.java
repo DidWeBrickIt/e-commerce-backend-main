7b64900ab9edab72953d2f7f3e1bfb53d9d98802
Jouosh <joshua.martin.2016@gmail.com>
2022-09-14 15:15:27 -0400

    Creates tests for both methods in jwt service

---
+++ src/test/java/com/revature/JwtServiceTests.java
@@ -0,0 +1,28 @@
+package com.revature;
+
+import com.revature.annotations.AuthRestriction;
+import com.revature.dtos.Jwt;
+import com.revature.services.JwtService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JwtServiceTests {
+
+    @Autowired
+    JwtService jwtService;
+
+    @Test
+    public void create_jwt_test() {
+        Jwt jwt = this.jwtService.createJWT("test@test", AuthRestriction.USER);
+        Assertions.assertTrue(this.jwtService.validateJWT(jwt.getJwt()));
+    }
+
+    @Test
+    public void validate_invalid_jwt_test() {
+        Assertions.assertThrows(RuntimeException.class, () -> this.jwtService.validateJWT("aaadlanfsdnvoisdn"));
+    }
+
+}
