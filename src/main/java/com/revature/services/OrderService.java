3451a9ecf01b9edca5c087d3bac511d0501510b6
jalec <jaleckc@gmail.com>
2022-09-16 09:16:39 -0700

    View Previous Order functionality

---
+++ src/main/java/com/revature/services/OrderService.java
@@ -0,0 +1,23 @@
+package com.revature.services;
+
+import com.revature.models.Order;
+import com.revature.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> addOrders(List<Order> orderList){return this.orderRepository.saveAll(orderList);}
+    public List<Order> findAllOrderByUserId(int id){
+        return this.orderRepository.findByUserId(id);
+    }
+}
