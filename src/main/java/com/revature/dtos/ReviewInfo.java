b178e7b111f0c736cbb9de437e662fcd7b80397d
Sunny <faronwmoore@gmail.com>
2022-09-26 15:40:40 -0500

    Removed unused imports

--- src/main/java/com/revature/dtos/ReviewInfo.java
+++ src/main/java/com/revature/dtos/ReviewInfo.java
@@ -4,11 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
